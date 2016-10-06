39678bfc7e8df59b84f4db31051a8e2b1a7f52bc
mortobot <mortona500@hotmail.com>
2016-10-06 20:00:26 +0100

    Initial String Calculator Tests
    Tests for empty string equals zero and sring of one equals one.

--- String Calculator Test/String Calculator Test/UnitTest1.cs
+++ String Calculator Test/String Calculator Test/UnitTest1.cs
@@ -7,8 +7,35 @@ namespace String_Calculator_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EmptyStringEqualsZero()
         {
+            //given someone enters an empty string
+            var stringToBeEntered = "";
+            //when I convert string to int
+            var result = CalculateString(stringToBeEntered);
+            //then String Calculator returns 0
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void StringOfOneEqualsOne()
+        {
+            //given someone enters an empty string
+            var stringToBeEntered = "1";
+            //when I convert string to int
+            var result = CalculateString(stringToBeEntered);
+            //then String Calculator returns 0
+            Assert.AreEqual(1, result);
+        }
+
+        public int CalculateString(string numberString)
+        {
+            if (numberString == "1")
+            {
+                return 1;
+            }
+            return 0;
         }
     }
+
 }
