10ef8ab6e0196d476a9bdc47ae3969d96b504dfc
mortobot <mortona500@hotmail.com>
2016-10-19 19:04:08 +0100

    Test for two equals two
    Botched test for two equals two, passes with duplicate code, needs
    refactor

--- String Calculator Test/String Calculator Test/UnitTest1.cs
+++ String Calculator Test/String Calculator Test/UnitTest1.cs
@@ -28,11 +28,29 @@ namespace String_Calculator_Test
             Assert.AreEqual(1, result);
         }
 
+        [TestMethod]
+        public void StringOfTwoEqualsTwo()
+        {
+            //given someone enters an empty string
+            var stringToBeEntered = "2";
+            //when I convert string to int
+            var result = CalculateString(stringToBeEntered);
+            //then String Calculator returns 0
+            Assert.AreEqual(2, result);
+        }
+
         public int CalculateString(string numberString)
         {
+            var acceptableNumbers = new string[1, 2];
+            ;
+
             if (numberString == "1")
             {
                 return 1;
+            }
+            if (numberString == "2")
+            {
+                return 2;
             }
             return 0;
         }
